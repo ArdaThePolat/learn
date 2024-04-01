9b074ba47dd4d95fbb5294c6d3f53b98cd085a9a
“Arda <ardaapol.t@gmail.com>
2024-04-01 22:00:49 +0300

    backup-commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import './App.css';
+import { useState } from "react";
 
-function MyButton(){
-  return(
-    <button>Just a button</button>
+export default function Board() {
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return (
+    <>
+    <div className="board-row">
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+    </div>
+    <div className="board-row">
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+    </div>
+    <div className="board-row">
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+    </div>
+    </>
   )
 }
 
-export default function App(){
-  return(
-    <div>
-      <h1 className='App-header'>Welcome to the app!</h1>
-      <MyButton/>
-    </div>
-  )
+function Square({value, onSquareClick}) {
+  return (
+  <button className="square" onClick={onSquareClick}>
+    {value}
+  </button>
+  );
 }
